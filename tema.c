922d5e6881bb8122518ee5db5de27aea44b65935
DavidGG13 <davidgheorghe398@gmail.com>
2023-04-13 16:34:32 +0300

    Update3

--- tema.c
+++ tema.c
@@ -26,12 +26,12 @@ typedef struct Player Player;
 void citireF(FILE *f, lstTeam *l) {
   int n, i, j;
   fscanf(f, "%d", &n);
-  //printf("Numarul de echipe este: %d\n", n);
+  // printf("Numarul de echipe este: %d\n", n);
   for (i = 0; i < n; i++) {
-    
+
     fscanf(f, "%d", &(l->T.numPlayers));
     l->T.P = (Player *)malloc(sizeof(Player) * l->T.numPlayers);
-    
+
     char *buffer;
     buffer = (char *)malloc(sizeof(char) * 100);
     fgetc(f);
@@ -39,9 +39,10 @@ void citireF(FILE *f, lstTeam *l) {
     l->T.name = (char *)malloc(sizeof(char) * strlen(buffer));
     strcpy(l->T.name, buffer);
     free(buffer);
-    
-    //printf("Nume echipa: %s  Numar membrii: %d\n", l->T.name, l->T.numPlayers);
-    
+
+    // printf("Nume echipa: %s  Numar membrii: %d\n", l->T.name,
+    // l->T.numPlayers);
+
     for (j = 0; j < l->T.numPlayers; j++) {
 
       buffer = (char *)malloc(sizeof(char) * 100);
@@ -57,9 +58,10 @@ void citireF(FILE *f, lstTeam *l) {
       free(buffer);
 
       fscanf(f, "%d", &(l->T.P[j].points));
-      //printf("Nume: %s  Prenume: %s  Scor: %d\n", l->T.P[j].secondName, l->T.P[j].firstName, l->T.P[j].points);
+      // printf("Nume: %s  Prenume: %s  Scor: %d\n", l->T.P[j].secondName,
+      // l->T.P[j].firstName, l->T.P[j].points);
     }
-    
+
     lstTeam *p;
     p = (lstTeam *)malloc(sizeof(lstTeam));
     l->next = p;
@@ -67,27 +69,28 @@ void citireF(FILE *f, lstTeam *l) {
     l = l->next;
     l->next = NULL;
   }
-  l=l->prev;
-  l->next=NULL;
+  l = l->prev;
+  l->next = NULL;
   fclose(f);
 }
 
-
-void scriereF (FILE *f, lstTeam *l){
-  while(l->next!=NULL){ 
-   l=l->next;
-   }
-  while(l!=NULL){ 
-   fputs(l->T.name, f);
-   l=l->prev;
-   }
+void scriereF(FILE *f, lstTeam *l) {
+  while (l->next != NULL) {
+    l = l->next;
+  }
+  while (l != NULL) {
+    fputs(l->T.name, f);
+    l = l->prev;
+  }
 }
 
 int main() {
-  FILE *d; 
+  FILE *d;
   FILE *r;
   FILE *c;
-  if ((d = fopen("date/t1/d.in", "r+t")) == NULL ||(r = fopen("out/out1.out", "w+t") ) == NULL || (c = fopen("date/t1/c.in", "r+t")) == NULL) {
+  if ((d = fopen("date/t1/d.in", "r+t")) == NULL ||
+      (r = fopen("out/out1.out", "w+t")) == NULL ||
+      (c = fopen("date/t1/c.in", "r+t")) == NULL) {
     printf("Unul sau ambele fisiere nu poate/pot fi deschis/e !\n");
     exit(1);
   }
@@ -95,7 +98,7 @@ int main() {
   lista = (lstTeam *)malloc(sizeof(lstTeam));
   lista->next = NULL;
   lista->prev = NULL;
-  
+
   citireF(d, lista);
   scriereF(r, lista);
 }
