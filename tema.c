27da59c52bfe7add23c9bd4bffebd23867efeb78
DavidGG13 <davidgheorghe398@gmail.com>
2023-04-14 14:16:30 +0300

    update2.1

--- tema.c
+++ tema.c
@@ -88,12 +88,84 @@ void scriereF(FILE *f, lstTeam *l) {
   }
 }
 
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+  int pivot = array[high];  // select the rightmost element as pivot
+                            // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(array, low, high);
+
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
+}
+
+void delNode(lstTeam *l) {}
+
 void Eliminate(lstTeam *l, int n) {
-  int i;
+  int i, m = 1, diff;
   float v[n];
-  lstTeam *p = l;
+  lstTeam *p = l, *q;
   for (i = 0; i < n; i++, p = p->next) {
     v[i] = p->T.teamScore;
+  }
+
+  quickSort(v, 0, n - 1);
+
+  while (m < n) {
+    m = m * 2;
+  }
+  m = m / 2;
+  diff = n - m;
+  m=diff;
+  i=0;
+  diff = v[diff - 1];
+  p = l;
+  while (p) {
+    if (diff >= p->T.teamScore && i<diff) {
+      lstTeam *temp;
+      temp=p;
+      q=p->prev;
+      p=p->next;
+      q->next=p;
+      p->prev=q;
+      free(temp);
+    }
   }
 }
 
